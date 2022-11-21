68051c61e708e72a12f560737f0d3c7268889f7b
Maeght Loan <37497007+Qypol342@users.noreply.github.com>
2022-11-21 08:50:08 +0000

    good start

--- test.py
+++ test.py
@@ -1,9 +1,27 @@
 # pip3 install Mastodon.py
+# pip install python-dotenv
 from mastodon import Mastodon
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
+"""
 Mastodon.create_app(
      'SpaceNews',
      api_base_url = 'https://masto.ai',
      to_file = 'pytooter_clientcred.secret'
+)
+"""
+
+
+
+mastodon = Mastodon(
+    client_id = 'pytooter_clientcred.secret',
+    api_base_url = 'https://masto.ai'
+)
+mastodon.log_in(
+    'actunewspace@gmail.com',
+    os.getenv('KEY'),
+    to_file = 'pytooter_usercred.secret'
 )
